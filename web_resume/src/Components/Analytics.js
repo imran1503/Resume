cf19b8369ef63ec31c362856f9ba3b4dae221bf3
Imran Latif <imran.latif1503@gmail.com>
2026-03-11 17:11:14 -0400

    Cookie error

--- web_resume/src/Components/Analytics.js
+++ web_resume/src/Components/Analytics.js
@@ -1,7 +1,9 @@
 import ReactGA from "react-ga4";
 
 export const initGA = () => {
-  ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+  ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID, {
+    cookie_domain: "imran1503.github.io",
+  });
 };
 
 export const trackPage = (path) => {
