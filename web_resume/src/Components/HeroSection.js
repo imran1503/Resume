c61f226148f9d5804ccb33dbe303f9a992548530
Imran Latif <imran.latif1503@gmail.com>
2026-03-11 14:25:17 -0400

    Adjusted hero section for mobile

--- web_resume/src/Components/HeroSection.js
+++ web_resume/src/Components/HeroSection.js
@@ -8,56 +8,60 @@ import { Button } from "../Components/Button.js";
 function HeroSection() {
   return (
     <div className="hero-container">
+      <div className="hero-content">
+        <div className="hero-image-wrapper">
+          <img
+            src={process.env.PUBLIC_URL + "/images/ILPhoto2.jpg"}
+            alt="Imran Latif"
+            className="hero-image"
+          />
+        </div>
 
-      <div className="hero-image-wrapper">
-        <img
-          src={process.env.PUBLIC_URL + "/images/ILPhoto2.jpg"}
-          alt="Imran Latif"
-          className="hero-image"
-        />
-      </div>
+        <div className="banner-text">
+          <h1 className="headline">Hi, </h1>
+          <h1 className="headline"> I'm Imran </h1>
 
-      <div className="banner-text">
-        <h1 className="headline">Hi, </h1>
-        <h1 className="headline"> I'm Imran </h1>
-        <h3 className="subheadline">
-          Excited full stack developer in the making!
-        </h3>
-        <h3 className="subheadline"> Welcome to my portfolio site </h3>
+          <h3 className="subheadline">
+            Excited full stack developer in the making!
+          </h3>
 
-        <Button
-          className="hero-btns"
-          buttonStyle={"btn--outline"}
-          buttonSize={"btn--medium"}
-          MyLink={
-            "https://drive.google.com/file/d/1vO3p3PZfHaGAeXfI8Syg4Mcw7mXZJYD-/view?usp=drive_link"
-          }
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            style={{
-              width: "1.2rem",
-              height: "1.2rem",
-              marginRight: "8px",
-              verticalAlign: "middle",
-            }}
+          <h3 className="subheadline">Welcome to my portfolio site</h3>
+
+          <Button
+            className="hero-btns"
+            buttonStyle={"btn--outline"}
+            buttonSize={"btn--medium"}
+            MyLink={
+              "https://drive.google.com/file/d/1KJRAu3PTc7RxllWXLDRtsF1J3Ye0RQav/view?usp=drive_link"
+            }
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 16.5V18a2.25 2.25 0 002.25 2.25h13.5A2.25
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              style={{
+                width: "1.2rem",
+                height: "1.2rem",
+                marginRight: "8px",
+                verticalAlign: "middle",
+              }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5V18a2.25 2.25 0 002.25 2.25h13.5A2.25
                         2.25 0 0021 18v-1.5M7.5 12l4.5 4.5m0 0l4.5-4.5m-4.5
                         4.5V3"
-            />
-          </svg>
-          Download my Resume!
-        </Button>
+              />
+            </svg>
+            Download my Resume!
+          </Button>
+        </div>
       </div>
     </div>
   );
 }
+
 export default HeroSection;
